12276129b06f8950382b58c8031044d2f660ca61
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-11 00:29:58 +0530

    Added Employee wage calculate

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -9,6 +9,10 @@ namespace EmpWage
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             //Constants
             int IS_FULL_TIME = 1;
+            int Emp_RATE_PER_TIME = 20;
+            //Variables
+            int empHrs = 0;
+            int empwage = 0;
             //It is using to generate random numbers
             Random random = new Random();
             //Computation
@@ -16,11 +20,17 @@ namespace EmpWage
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empwage = empHrs * Emp_RATE_PER_TIME;
+            Console.WriteLine("EmpWage : " + empwage);
         }
     }
 }
+
+
