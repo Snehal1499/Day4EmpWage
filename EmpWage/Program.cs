bb3c5dec215eb184d56afa12b6e5404c93a3a08f
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-11 11:35:42 +0530

    Added Wages For A Month

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,34 +1,47 @@
 ﻿using System;
-
+//UC5_Calculating Wages For A Month
 namespace EmpWage
 {
     internal class Program
     {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int Emp_RATE_PER_TIME = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //Constants
-            int IS_FULL_TIME = 1;
-            int Emp_RATE_PER_TIME = 20;
             //Variables
             int empHrs = 0;
             int empwage = 0;
-            //It is using to generate random numbers
-            Random random = new Random();
+            int totalEmpWage = 0;
+
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                //It is using to generate random numbers
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);// 0,1,2
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+
+                }
+                empwage = empHrs * Emp_RATE_PER_TIME;
+                totalEmpWage += empwage;
+                Console.WriteLine("EmpWage : " + empwage);
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
-            empwage = empHrs * Emp_RATE_PER_TIME;
-            Console.WriteLine("EmpWage : " + empwage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
