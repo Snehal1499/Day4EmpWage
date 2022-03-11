5714da69f759bfea504f982a8d87608ed5a26479
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-11 11:02:46 +0530

    Added Switch Case

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,36 +1,47 @@
 ﻿using System;
-
+//Switch Case 
 namespace EmpWage
 {
     internal class Program
     {
+
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int Emp_RATE_PER_TIME = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //Constants
-            int IS_FULL_TIME = 1;
-            int Emp_RATE_PER_TIME = 20;
             //Variables
             int empHrs = 0;
             int empwage = 0;
+
             //It is using to generate random numbers
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
+
             empwage = empHrs * Emp_RATE_PER_TIME;
             Console.WriteLine("EmpWage : " + empwage);
         }
+
     }
 }
+
+
+
 
 
